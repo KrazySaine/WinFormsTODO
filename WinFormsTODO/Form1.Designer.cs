739cd82de4804e37a5b98f2127321b5d9da4974e
KrazySaine <canecr87@gmail.com>
2025-01-16 10:07:33 -0800

    added forms, need to add classes and do backend work.

--- WinFormsTODO/Form1.Designer.cs
+++ WinFormsTODO/Form1.Designer.cs
@@ -28,31 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnAddTODO = new Button();
+            txtTask = new TextBox();
+            Task = new Label();
+            DueDate = new Label();
+            txtDueDate = new TextBox();
+            btnClear = new Button();
+            gbTaskForm = new GroupBox();
+            gbTaskList = new GroupBox();
+            gbTaskForm.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // btnAddTODO
             // 
-            button1.Location = new Point(369, 150);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnAddTODO.Location = new Point(322, 126);
+            btnAddTODO.Name = "btnAddTODO";
+            btnAddTODO.Size = new Size(75, 23);
+            btnAddTODO.TabIndex = 0;
+            btnAddTODO.Text = "Add TODO";
+            btnAddTODO.UseVisualStyleBackColor = true;
+            // 
+            // txtTask
+            // 
+            txtTask.Location = new Point(131, 40);
+            txtTask.Name = "txtTask";
+            txtTask.Size = new Size(266, 23);
+            txtTask.TabIndex = 1;
+            // 
+            // Task
+            // 
+            Task.AutoSize = true;
+            Task.Location = new Point(73, 40);
+            Task.Name = "Task";
+            Task.Size = new Size(30, 15);
+            Task.TabIndex = 2;
+            Task.Text = "Task";
+            Task.Click += label1_Click;
+            // 
+            // DueDate
+            // 
+            DueDate.AutoSize = true;
+            DueDate.Location = new Point(73, 94);
+            DueDate.Name = "DueDate";
+            DueDate.Size = new Size(55, 15);
+            DueDate.TabIndex = 3;
+            DueDate.Text = "Due Date";
+            // 
+            // txtDueDate
+            // 
+            txtDueDate.Location = new Point(131, 94);
+            txtDueDate.Name = "txtDueDate";
+            txtDueDate.Size = new Size(266, 23);
+            txtDueDate.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(241, 126);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 5;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // gbTaskForm
+            // 
+            gbTaskForm.Controls.Add(txtTask);
+            gbTaskForm.Controls.Add(btnClear);
+            gbTaskForm.Controls.Add(btnAddTODO);
+            gbTaskForm.Controls.Add(txtDueDate);
+            gbTaskForm.Controls.Add(Task);
+            gbTaskForm.Controls.Add(DueDate);
+            gbTaskForm.Location = new Point(138, 26);
+            gbTaskForm.Name = "gbTaskForm";
+            gbTaskForm.Size = new Size(506, 163);
+            gbTaskForm.TabIndex = 6;
+            gbTaskForm.TabStop = false;
+            gbTaskForm.Text = "Task Form";
+            // 
+            // gbTaskList
+            // 
+            gbTaskList.Location = new Point(138, 228);
+            gbTaskList.Name = "gbTaskList";
+            gbTaskList.Size = new Size(506, 202);
+            gbTaskList.TabIndex = 7;
+            gbTaskList.TabStop = false;
+            gbTaskList.Text = "Task List";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(gbTaskList);
+            Controls.Add(gbTaskForm);
             Name = "Form1";
             Text = "Form1";
+            gbTaskForm.ResumeLayout(false);
+            gbTaskForm.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnAddTODO;
+        private TextBox txtTask;
+        private Label Task;
+        private Label DueDate;
+        private TextBox txtDueDate;
+        private Button btnClear;
+        private GroupBox gbTaskForm;
+        private GroupBox gbTaskList;
     }
 }
